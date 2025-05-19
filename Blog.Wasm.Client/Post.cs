59bab0f52ebe5d4fabddb9f2b57b80ccc6c19329
Josh <Josh@DESKTOP-NTP2GFU>
2025-05-19 01:43:05 -0230

    Post page. Renders a single post from _posts

---
+++ Blog.Wasm.Client/Post.cs
@@ -0,0 +1,16 @@
+﻿namespace Blog.Wasm.Client
+{
+    public class Post
+    {
+        public string FileName { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public List<string> Tags { get; set; }
+        public string Content { get; set; }
+    }
+    public class Index
+    {
+        public List<Client.Post> Posts { get; set; }
+    }
+
+}
