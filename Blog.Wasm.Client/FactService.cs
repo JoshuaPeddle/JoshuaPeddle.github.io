98de8642920956b2a1c43f57d0325c72b43f80c4
Josh <Josh@DESKTOP-NTP2GFU>
2025-05-19 22:54:13 -0230

    Add random fact popup

---
+++ Blog.Wasm.Client/FactService.cs
@@ -0,0 +1,14 @@
+﻿namespace Blog.Wasm.Client;
+
+public class FactService(HttpClient httpClient)
+{
+    private readonly HttpClient _httpClient = httpClient;
+    private Random random = new Random();
+
+    public async Task<string> GetRandomFact()
+    {
+        var response = await _httpClient.GetStringAsync("facts.txt");
+        var lines = response.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+        return lines[random.Next(lines.Length)];
+    }
+}
--- Blog.Wasm.Client/Program.cs
+++ Blog.Wasm.Client/Program.cs
@@ -2,18 +2,32 @@ using Blog.Wasm.Client;
 using Markdig;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-builder.Services.AddMudServices();
-
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
+
+    config.SnackbarConfiguration.PreventDuplicates = false;
+    config.SnackbarConfiguration.NewestOnTop = false;
+    config.SnackbarConfiguration.ShowCloseIcon = false;
+    config.SnackbarConfiguration.VisibleStateDuration = 10000;
+    config.SnackbarConfiguration.HideTransitionDuration = 500;
+    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+    config.SnackbarConfiguration.BackgroundBlurred = true;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Text;
+});
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(sp => new MarkdownPipelineBuilder()
                                     .UseAdvancedExtensions()
                                     .Build());
+builder.Services.AddScoped<FactService>();
 
 await builder.Build().RunAsync();
